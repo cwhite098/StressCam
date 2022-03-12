9eabbbe023f9e5cbd1be8dc4b919c926436b991d
cwhite098 <gd19031@bristol.ac.uk>
2022-03-12 16:18:55 +0000

    added nose coords to video feed

--- feed.py
+++ feed.py
@@ -12,18 +12,27 @@ while(True):
     ret, frame = cap.read()
 
     with mp_holistic.Holistic(
-            static_image_mode=False, model_complexity=1, enable_segmentation=False,
+            static_image_mode=False, model_complexity=0, enable_segmentation=False,
             refine_face_landmarks=False) as holistic:
 
+        
         image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image_hight, image_width, _ = image.shape
+        image.flags.writeable = False
         image_recolored = holistic.process(image)
 
-        mp_drawing.draw_landmarks(image, image_recolored.face_landmarks, mp_holistic.FACEMESH_TESSELATION)
+        #mp_drawing.draw_landmarks(image, image_recolored.face_landmarks, mp_holistic.FACEMESH_TESSELATION)
         mp_drawing.draw_landmarks(image, image_recolored.pose_landmarks, mp_holistic.POSE_CONNECTIONS)
-        mp_drawing.draw_landmarks(image, image_recolored.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
-        mp_drawing.draw_landmarks(image, image_recolored.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+        #mp_drawing.draw_landmarks(image, image_recolored.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+        #mp_drawing.draw_landmarks(image, image_recolored.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
 
-        cv2.imshow('frame', image)
+        print(
+          f'Nose coordinates: ('
+          f'{image_recolored.pose_landmarks.landmark[mp_holistic.PoseLandmark.NOSE].x*image_width}, '
+          f'{image_recolored.pose_landmarks.landmark[mp_holistic.PoseLandmark.NOSE].y*image_hight})'
+      )
+
+        cv2.imshow('frame', cv2.flip(image, 1))
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
