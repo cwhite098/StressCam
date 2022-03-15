adfc492df5af4f283b21c6ce64e90e444b272d02
cwhite098 <gd19031@bristol.ac.uk>
2022-03-15 11:21:21 +0000

    added POS signal for heart rate

--- heart_rate.py
+++ heart_rate.py
@@ -1,12 +1,5 @@
-"""
-Webcam Heart Rate Monitor
-Gilad Oved
-December 2018
-"""
-
 import numpy as np
 import cv2
-import sys
 import matplotlib.pyplot as plt
 
 class Heart_Rate_Monitor:
@@ -21,7 +14,7 @@ class Heart_Rate_Monitor:
         self.videoHeight = 120
 
         self.videoChannels = 3
-        self.videoFrameRate = 15
+        self.videoFrameRate = 20
 
         # Color Magnification Parameters
         self.levels = 2
@@ -44,6 +37,7 @@ class Heart_Rate_Monitor:
         # Initialize Gaussian Pyramid
         self.firstFrame = np.zeros((self.videoHeight, self.videoWidth, self.videoChannels))
         self.firstGauss = self.buildGauss(self.firstFrame, self.levels+1)[self.levels]
+        # 4D array: time, video_x, video_y, RGB channels
         self.videoGauss = np.zeros((self.bufferSize, self.firstGauss.shape[0], self.firstGauss.shape[1], self.videoChannels))
         self.fourierTransformAvg = np.zeros((self.bufferSize))
 
@@ -57,26 +51,37 @@ class Heart_Rate_Monitor:
         self.bpmBufferSize = 10
         self.bpmBuffer = np.zeros((self.bpmBufferSize))
 
-        self.i = 0
+        self.project_mat = [[0, 1, -1],
+                            [-2, 1, 1]]
 
-     
-        self.fig, self.axes = plt.subplots(constrained_layout = True, nrows=2 , ncols=1)
+        self.i = 0
+        self.h = np.linspace(-1,1,150)
+
+        # Create figure with 2 plots
+        self.fig, self.axes = plt.subplots(constrained_layout = True, nrows=3 , ncols=1)
         self.ax1 = self.axes[0]
         self.ax2 = self.axes[1]
-
+        self.ax3 = self.axes[2]
+        # Set params for BPM graph
         self.ax1.set_ylim([50,120]), self.ax1.set_xlabel('Time'), self.ax1.set_ylabel('BPM')
         self.ax1.set_title('Beats Per Minute')
-
+        # Set params for frequency spectrum
         self.ax2.set_ylim([0,30]), self.ax2.set_xlabel('Frequency'), self.ax2.set_ylabel('Magnitude')
         self.ax2.set_title('Fourier Transform')
-
+        # Set params for POS signal
+        self.ax3.set_ylim([-0.01,0.01]), self.ax3.set_xlabel('Time'), self.ax3.set_ylabel('POS Signal')
+        self.ax3.set_title('POS Signal')
+        # Set the lines to be used for the plots
         self.spec = self.ax2.plot(self.frequencies, self.fourierTransformAvg, animated=True)[0]
         self.line = self.ax1.plot(np.linspace(0,100,100),np.zeros(100), animated=True)[0]
+        self.linePOS = self.ax3.plot(np.linspace(-1,1,150), np.zeros(150), animated=True)[0]
         self.fig.show()
         self.fig.canvas.draw()
         self.background1 = self.fig.canvas.copy_from_bbox(self.ax1.bbox)
         self.background2 = self.fig.canvas.copy_from_bbox(self.ax2.bbox)
+        self.background3 = self.fig.canvas.copy_from_bbox(self.ax3.bbox)
 
+        # Lists to store bpm data
         self.bpm_list = []
         self.total_bpm_list = []
 
@@ -104,18 +109,20 @@ class Heart_Rate_Monitor:
         
         self.fig.canvas.restore_region(self.background1)
         self.fig.canvas.restore_region(self.background2)
+        self.fig.canvas.restore_region(self.background3)
         self.line.set_ydata(self.bpm_list)
-        self.spec.set_ydata(self.fourierTransformAvg)
+        self.spec.set_ydata(np.real(self.POS_fourier))
+        self.linePOS.set_ydata(self.h)
  
         self.ax1.draw_artist(self.line)
         self.ax2.draw_artist(self.spec)
+        self.ax3.draw_artist(self.linePOS)
         self.fig.canvas.blit(self.ax1.bbox)
         self.fig.canvas.blit(self.ax2.bbox)
-
+        self.fig.canvas.blit(self.ax3.bbox)
 
 
     def get_bpm(self, frame, start_tuple, rect_dims):
-
 
         # Positions the detection box (just where the data is collected from, doesn't move box in display)
         detectionFrame = frame[start_tuple[0]:start_tuple[0]+self.videoHeight, start_tuple[1]:start_tuple[1]+self.videoWidth, :]
@@ -123,6 +130,10 @@ class Heart_Rate_Monitor:
         # Construct Gaussian Pyramid
         # add it to the videoGauss buffer
         self.videoGauss[self.bufferIndex] = self.buildGauss(detectionFrame, self.levels+1)[self.levels]
+
+        # Get the average colour signals from buffer
+        C = self.videoGauss.mean((1,2))
+
         # Take the fourier transform of the frames in the buffer
         fourierTransform = np.fft.fft(self.videoGauss, axis=0)
 
@@ -132,11 +143,28 @@ class Heart_Rate_Monitor:
         # Grab a Pulse
         if self.bufferIndex % self.bpmCalculationFrequency == 0:
             self.i = self.i + 1
+
+            # temporal normalisation for C ???
+            mean = C.mean(1)
+            C[:,0] = C[:,0] / mean
+            C[:,1] = C[:,1] / mean
+            C[:,2] = C[:,2] / mean
+
+            S = np.matmul(self.project_mat, np.transpose(C))
+
+            coeff = (np.std(S[0,:])/np.std(S[1,:]))
+            self.h = S[0,:] + (coeff*S[1,:])
+            self.h = self.h - np.mean(self.h)
+            print(self.h)
+            self.POS_fourier = np.fft.fft(self.h)
+            self.POS_fourier[self.mask == False] = 0
+
             for buf in range(self.bufferSize):
                 # Average the fourier transform for each frame
                 self.fourierTransformAvg[buf] = np.real(fourierTransform[buf]).mean()
             # Get the max frequency
-            hz = self.frequencies[np.argmax(self.fourierTransformAvg)]
+            #hz = self.frequencies[np.argmax(self.fourierTransformAvg)]
+            hz = self.frequencies[np.argmax(self.POS_fourier)]
             bpm = 60.0 * hz
             self.bpmBuffer[self.bpmBufferIndex] = bpm
             self.bpmBufferIndex = (self.bpmBufferIndex + 1) % self.bpmBufferSize
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from heart_rate import Heart_Rate_Monitor
 import mediapipe as mp
 import cv2
 import numpy as np
+import time
 
 
 mp_drawing = mp.solutions.drawing_utils
@@ -11,6 +12,7 @@ mp_face = mp.solutions.face_detection
 cap = cv2.VideoCapture()
 # The device number might be 0 or 1 depending on the device and the webcam
 cap.open(0, cv2.CAP_DSHOW)
+cap.set(cv2.CAP_PROP_FPS, 20)
 
 realWidth = 640
 realHeight = 480
@@ -18,18 +20,23 @@ realHeight = 480
 cap.set(3, realWidth);
 cap.set(4, realHeight);
 
+# Record frame times for fps counter
+prev_frame_time = 0
+new_frame_time = 0
+
 HRM = Heart_Rate_Monitor()
 
 while True:
+
 	# Read the output from the webcam
 	ret, image = cap.read()
+
 	# init model
 	with mp_face.FaceDetection(
 		model_selection=0, min_detection_confidence=0.5) as face_detection: 
 
 		# Change the colours (not sure why)
 		#image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-		image_hight, image_width, _ = image.shape
 		image.flags.writeable = True
 
 		# Detect the face
@@ -53,6 +60,11 @@ while True:
 										(int(rel_height*realHeight), int(rel_width*realWidth)))
 		
 
+		# Finished processing, record frame time
+		new_frame_time = time.time()
+		fps = 1/(new_frame_time-prev_frame_time)
+		prev_frame_time = new_frame_time
+		print(int(fps))
 
 		cv2.imshow('frame', frame)
 
