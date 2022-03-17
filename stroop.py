ffaf4a256bc23bd5c1f49be201b8fcb9314d1117
finngueterbock <wu19361@bristol.ac.uk>
2022-03-17 14:47:21 +0000

    stroopy stroop working

--- stroop.py
+++ stroop.py
@@ -1,9 +1,10 @@
+import tkinter as tk
+import random
 
 def stroop_test():
-    import tkinter as tk
-    import random
+
     global colour, score, word, attempts
-    colours = {'red':'r', 'blue':'b', 'green':'g', 'yellow':'y', 'orange':'o', 'purple':'p'}
+    colours = {'red':'r', 'blue':'b', 'green':'g', 'yellow':'y'}
     def stimulus(same):
         word = random.choice(list(colours))
 
@@ -15,14 +16,16 @@ def stroop_test():
         return word, colour
 
     def update_score(event):
+        global attempts, score
         attempts += 1
         if colours[colour] == event.char:
             score += 1
-
+        print(colours[colour], event.char)
         print(f'Score:{score}/{attempts}')
         next_selected()
 
     def next_selected():
+        global word, colour
         word, colour = stimulus(random.choices([True, False], weights=(0.3, 0.7))[0])
         label.config(text=word, fg=colour)
         label.update()
@@ -33,6 +36,7 @@ def stroop_test():
 
     root = tk.Tk()
     score = 0
+    attempts = 0
 
     # create label using stimulus
     word, colour = stimulus(False)
@@ -44,3 +48,6 @@ def stroop_test():
 
     root.bind('<Key>', lambda event,: update_score(event))
     root.mainloop()
+
+if __name__ == '__main__':
+    stroop_test()
