818bb323441a27642211def93859873ae3482cd3
finngueterbock <wu19361@bristol.ac.uk>
2022-03-22 14:09:46 +0000

    stroop timer working properly

--- stroop.py
+++ stroop.py
@@ -1,59 +1,71 @@
-import tkinter as tk
+from tkinter import *
 import random
+from time import perf_counter, sleep
+from threading import Timer, Thread
+from PIL import ImageTk, Image
+from icecream import ic
 
 class stroop_test:
 
 
-    def __init__(self, difficulty=1):
+    def __init__(self, timeout=2):
 
-        self.colours = {'red':'r', 'blue':'b', 'green':'g', 'yellow':'y'}
+        self.timeout = timeout
         self.score = 0
-        # self.same = False
         self.attempts = 0
-        self.same = random.choices([True, False], weights=(0.3, 0.7))[0]
+        self.colours = {'red':'r', 'blue':'b', 'green':'g', 'yellow':'y'}
+        self.root = Tk()
+        self.times_pressed = 0
         self.word, self.colour = self.stimulus()
-        self.root = tk.Tk()
-        self.label = tk.Label(self.root, text=self.word, fg=self.colour, font=("Comic Sans MS", 100), width =10, height=4)
+        self.label = Label(self.root, text=self.word, fg=self.colour, font=("Comic Sans MS", 200), width =8, height=2)
         self.label.pack()
-
-        self.word, self.colour = self.stimulus()
-        self.run()
+        self.img = PhotoImage(file='big-red-cross.png')
+        self.image = Label(self.root, image=self.img, width =8000, height=2000)
 
     def stimulus(self):
+        self.times_pressed = 0
         self.word = random.choice(list(self.colours))
-
+        self.same = random.choices([True, False], weights=(0.3, 0.7))[0]
         if self.same:
             return (self.word, self.word)
-
         list(self.colours).remove(self.word)
         self.colour = random.choice(list(self.colours))
         return self.word, self.colour
 
-    def update_score(self, event):
-        self.attempts += 1
-        if self.colours[self.colour] == event.char:
-            self.score += 1
+    def key_pressed(self, event):
+        self.times_pressed += 1
+        print(self.times_pressed)
+        if self.times_pressed == 1:
+            if self.colours[self.colour] == event.char:
+                self.score += 1
+                self.attempts += 1
+            else:
+                self.attempts += 1
+                self.image = Label(self.root, image=self.img, width =8000, height=2000)
+                self.image.place(relx=0.5, rely=0.5, anchor='center')
+
         print(self.colours[self.colour], event.char)
         print(f'Score:{self.score}/{self.attempts}')
-        self.next_selected(self.label)
 
-    def next_selected(self, label):
+    def new_question(self):
+        if self.image.winfo_exists():
+            self.image.destroy()
         self.word, self.colour = self.stimulus()
-        label.config(text=self.word, fg=self.colour)
-        label.update()
-        return
+        self.label.config(text=self.word, fg=self.colour)
+        self.label.update()
+
+        self.root.after((self.timeout)*1000, self.new_question)
 
     def quit_selected(self):
         self.root.destroy()
 
     def run(self):
-        self.closebutton = tk.Button(self.root, text='close', command=self.quit_selected)
+        self.closebutton = Button(self.root, text='close', command=self.quit_selected)
         self.closebutton.pack(padx=50, pady=50)
-        self.root.bind('<Key>', lambda event,: self.update_score(event))
-
+        self.new_question()
+        self.root.bind('<Key>', lambda event,: self.key_pressed(event))
         self.root.mainloop()
-
-
 
 if __name__ == '__main__':
     st = stroop_test()
+    st.run()
