d40ca206b25f00dc78fd8188319d297ab3d67b4e
finngueterbock <wu19361@bristol.ac.uk>
2022-03-31 19:08:21 +0100

    can run stroop test and save video into data folder

--- stroop/stroop.py
+++ stroop/stroop.py
@@ -4,17 +4,19 @@ from time import perf_counter
 from playsound import playsound
 import multiprocessing
 
-class stroop_test:
 
+class StroopTest:
 
     def __init__(self, sound=False):
 
+        self.running = True
         self.sound = sound
         self.score = 0
         self.attempts = 0
 
-        self.colours = {'red':'r', 'blue':'b', 'green':'g', 'yellow':'y'}
-        self.wrong_texts = ['Oops!', 'Nope!', 'Not a chance!', 'Try Harder!', 'The COLOUR not the word...', 'Not good enough!', 'Is that it?!', 'Do better!', 'More!']
+        self.colours = {'red': 'r', 'blue': 'b', 'green': 'g', 'yellow': 'y'}
+        self.wrong_texts = ['Oops!', 'Nope!', 'Not a chance!', 'Try Harder!', 'The COLOUR not the word...',
+                            'Not good enough!', 'Is that it?!', 'Do better!', 'More!']
         self.right_texts = ['Yay!', 'Well done!', 'Easy does it...', 'Feeling stressed yet?', 'Good work!', 'Nice one!']
 
         self.root = Tk()
@@ -23,26 +25,26 @@ class stroop_test:
         self.times_pressed = 0
         self.word, self.colour = self.stimulus()
 
-        self.label = Label(self.root, text=self.word, fg=self.colour, font=("Comic Sans MS", 200), width =8, height=2)
+        self.label = Label(self.root, text=self.word, fg=self.colour, font=("Comic Sans MS", 200), width=8, height=2)
         self.label.pack()
 
         self.instructions = PhotoImage(file='stroop/instructions.png')
         self.cross = PhotoImage(file='stroop/big-red-cross.png')
         self.tick = PhotoImage(file='stroop/big-green-tick.png')
 
-        self.wrong = Label(self.root, image=self.cross, width =8000, height=2000)
-        self.right = Label(self.root, image=self.tick, width =8000, height=2000)
+        self.wrong = Label(self.root, image=self.cross, width=8000, height=2000)
+        self.right = Label(self.root, image=self.tick, width=8000, height=2000)
 
         self.wrong_text = Label(self.root, text=random.choice(self.wrong_texts), font=('Comic Sans MS', 40))
         self.right_text = Label(self.root, text=random.choice(self.right_texts), font=('Comic Sans MS', 40))
 
         self.score_label = Label(self.root, text=f'Score: {self.score} / {self.attempts}',
-                                 font=("Comic Sans MS", 50), width =12, height=1)
-        self.score_label.place(relx = 0.6, rely = 0)
+                                 font=("Comic Sans MS", 50), width=12, height=1)
+        self.score_label.place(relx=0.6, rely=0)
         self.start = True
 
         if self.sound:
-            self.sound_thread = multiprocessing.Process(target=playsound, args=('rising.mp3',))
+            self.sound_thread = multiprocessing.Process(target=playsound, args=('stroop/rising.mp3',))
             self.sound_thread.start()
 
     def start_screen(self):
@@ -77,19 +79,19 @@ class stroop_test:
             if self.colours[self.colour] == event.char:
                 self.score += 1
                 self.attempts += 1
-                self.right = Label(self.root, image=self.tick, width =800, height=500)
+                self.right = Label(self.root, image=self.tick, width=800, height=500)
                 self.right.place(relx=0.5, rely=0.5, anchor='center')
                 if self.mode == 'easy':
                     self.right_text = Label(self.root, text=random.choice(self.right_texts), font=('Comic Sans MS', 40))
                     self.right_text.place(relx=0.5, rely=0.85, anchor='center')
             else:
                 self.attempts += 1
-                self.wrong = Label(self.root, image=self.cross, width =800, height=500)
+                self.wrong = Label(self.root, image=self.cross, width=800, height=500)
                 self.wrong.place(relx=0.5, rely=0.5, anchor='center')
                 if self.mode == 'hard':
                     self.wrong_text = Label(self.root, text=random.choice(self.wrong_texts), font=('Comic Sans MS', 40))
                     self.wrong_text.place(relx=0.5, rely=0.85, anchor='center')
-        self.score_label.config(text =f'Score: {self.score} / {self.attempts}')
+        self.score_label.config(text=f'Score: {self.score} / {self.attempts}')
         self.score_label.update()
 
     def new_question(self, timeout, mode):
@@ -104,15 +106,17 @@ class stroop_test:
         if self.times_pressed == 0:
             if not self.start:
                 self.attempts += 1
-            self.score_label.config(text =f'Score: {self.score} / {self.attempts}')
+            self.score_label.config(text=f'Score: {self.score} / {self.attempts}')
             self.score_label.update()
         self.start = False
         self.word, self.colour = self.stimulus()
         self.label.config(text=self.word, fg=self.colour)
         self.label.update()
-        self.root.after((timeout)*1000, lambda: self.new_question(timeout, mode))
+        self.root.after((timeout) * 1000, lambda: self.new_question(timeout, mode))
 
     def quit_selected(self):
+        print('quitting')
+        self.running = False
         if self.sound:
             self.sound_thread.terminate()
         self.root.destroy()
@@ -123,13 +127,15 @@ class stroop_test:
             timeout = 3
         elif mode == 'hard':
             timeout = 1
-        self.closebutton = Button(self.root, text='close', command=self.quit_selected)
-        self.closebutton.pack(padx=50, pady=50)
+        self.close_button = Button(self.root, text='close', command=self.quit_selected)
+        self.close_button.pack(padx=50, pady=50)
         self.mode = mode
-        self.new_question(timeout, mode )
+        self.new_question(timeout, mode)
+        # self.root_bind('q')
         self.root.bind('<Key>', lambda event,: self.key_pressed(event))
 
         self.root.mainloop()
-if __name__ == '__main__':
-    st = stroop_test(sound=False)
-    st.start_screen()
+
+# if __name__ == '__main__':
+#     st = stroop_test(sound=False)
+#     st.start_screen()
